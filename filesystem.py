e34b8b755669e1294cdfbb2f9a2e42688f63a141
Davide Olgiati <davide.olgiati@hotmail.it>
2022-01-29 10:49:18 +0100

    Style fix in filesystem.py

--- filesystem.py
+++ filesystem.py
@@ -1,71 +1,80 @@
-import os   # os.path.relpath
+"""Module containing filesystem interface logic, used to move, copy and delete files"""
+
+import os  # os.path.relpath
 import shutil
 from watchdog.events import FileSystemEventHandler  # <--
 
-# la calsse 'MyHandler' serve per gestire gli eventi registrati
+
+# La classe 'MyHandler' serve per gestire gli eventi registrati
 # dalla libreria watchdog.
-# Estende la classe 'FileSystemEventHandler' che è la classe
-# più basilare presnte nella libreria, controlla ogni cambaimento
-# senza filtraggio
+# Estende la classe 'FileSystemEventHandler', controlla ogni
+# cambiamento senza distinzione
 
 
 class MyHandler(FileSystemEventHandler):
-    # nel costruttore salvo il valore di path, che rappresnta
+    """Main filesystem watchdog class"""
+    # nel costruttore salvo il valore di path, che rappresenta
     # la directory principale da osservare
-    def __init__(self, path, dest, setup):
+    def __init__(self, path, dst, setup):
+        """Class constructor, used to initialize the instance
+        with correct parameters"""
         self.path = path
-        self.dest = dest
-        self.dryrun = (setup & 2) == 2
+        self.dst = dst
+        self.dry_run = (setup & 2) == 2
         self.verbose = (setup & (1 << 3)) & (1 << 3)
         if (setup & 4) == 4:  # init flag attivo
             dirs = []
             files = []
-            dirs, files = self.dirWalk(path, dest, dirs, files)
-            dirs, files = self.dirWalk(dest, path, dirs, files)
+            dirs, files = self.dir_walk(path, dst, dirs, files)
+            dirs, files = self.dir_walk(dst, path, dirs, files)
             if self.verbose:
                 print("\nDIRS to be duplicated: ")
-            for dir in dirs:
-                test = os.path.isdir(dir)
+            for current_dir in dirs:
+                test = os.path.isdir(current_dir)
                 if not test:
                     if self.verbose:
-                        print("\t" + dir)
-                    os.mkdir(dir)
+                        print("\t" + current_dir)
+                    os.mkdir(current_dir)
 
             if self.verbose:
                 print("\nFILES to be duplicated: ")
-            for dst in files:
+            for dst_file in files:
                 src = os.path.join(path,
-                                   os.path.relpath(dst,
-                                                   dest))
-                test = os.path.isfile(dst)
-                if not test:
+                                   os.path.relpath(dst_file,
+                                                   dst))
+                if not os.path.isfile(dst_file):
                     if self.verbose:
-                        print("\t" + dst)
+                        print("\t" + dst_file)
                     shutil.copy2(src, dst)
 
-    def dirWalk(self, main, dest, dirs, files):
+    @staticmethod
+    def dir_walk(main, dst, dirs, files):
+        """Method used to explore a directory in order  to track file"""
         for dir_path, dir_names, file_names in os.walk(main):
-            for dir in dir_names:
-                new_path = os.path.join(dest, dir)
-                if (new_path not in dirs) and dir != ".":
+            for current_dir in dir_names:
+                new_path = os.path.join(dst, current_dir)
+                if (new_path not in dirs) and current_dir != ".":
                     dirs.append(new_path)
-            for f in file_names:
+            for _file in file_names:
                 rel_path = os.path.relpath(dir_path, main)
-                new_path = os.path.join(dest, rel_path)
+                new_path = os.path.join(dst, rel_path)
                 if rel_path == ".":
-                    new_file = os.path.join(dest, f)
+                    new_file = os.path.join(dst, _file)
                 else:
-                    new_file = os.path.join(new_path, f)
+                    new_file = os.path.join(new_path, _file)
                 files.append(new_file)
         return dirs, files
 
     # questo metodo è utilizzato unicamente per estrarre il
     # percorso relativo da un percorso assoluto, utilizzando
     # 'self.path'
-    def relativePath(self, full):
+    def relative_path(self, full):
+        """Wrapper method to extract relative path within a directory"""
         return os.path.relpath(full, self.path)
 
-    def log(self, paths, event):
+    @staticmethod
+    def log(paths, event):
+        """Static method used to log watchdog event"""
         if event == 'moved':
             print(paths[0], "moved to", paths[1])
         else:
@@ -73,50 +82,55 @@ class MyHandler(FileSystemEventHandler):
 
     # metodo principale, maggiori dettagli nel corpo
     def process(self, event):
+        """Main method, used to decide how to respond to an event tracked by
+        the watchdog"""
         # le due variabili locali che seguono hanno lo scopo
         # di rendere il codice più leggibile, ricopiano identici
         # i valori presenti nella struttura event
-        eventType = event.event_type
-        srcPath = self.relativePath(event.src_path)
-        if eventType == 'moved':
-            destPath = self.relativePath(event.dest_path)
-            self.log([self.relativePath(srcPath),
-                      self.relativePath(destPath)],
-                     eventType)
-            self.move(srcPath, destPath)
+        event_type = event.event_type
+        src_path = self.relative_path(event.src_path)
+        if event_type == 'moved':
+            dst_path = self.relative_path(event.dest_path)
+            self.log([self.relative_path(src_path),
+                      self.relative_path(dst_path)],
+                     event_type)
+            self.move(src_path, dst_path)
         else:
-            self.log([self.relativePath(srcPath)],
-                     eventType)
-            if not (not event.is_directory and eventType == 'created'):
-                self.modify(srcPath, eventType)
+            self.log([self.relative_path(src_path)],
+                     event_type)
+            if not (not event.is_directory and event_type == 'created'):
+                self.modify(src_path, event_type)
 
-    def move(self, srcPath, destPath):
-        oldPath = os.path.join(self.dest, srcPath)
-        newPath = os.path.join(self.dest, destPath)
-        shutil.move(oldPath, newPath)
+    def move(self, src_path, dst_path):
+        """File is moved action"""
+        old_path = os.path.join(self.dst, src_path)
+        new_path = os.path.join(self.dst, dst_path)
+        shutil.move(old_path, new_path)
 
-    def modify(self, srcPath, event):
-        source = os.path.join(self.path, srcPath)
-        dest = os.path.join(self.dest, srcPath)
+    def modify(self, src_path, event):
+        """File is modified action"""
+        source = os.path.join(self.path, src_path)
+        dst = os.path.join(self.dst, src_path)
         if event == 'creation' and os.path.isdir(source):
-            os.mkdir(dest)
+            os.mkdir(dst)
         elif event == 'delete':
-            os.remove(dest)
+            os.remove(dst)
         else:
-            shutil.copyfile(source, dest)
+            shutil.copyfile(source, dst)
 
     # metodo virtuale definito nella classe principale
-    # recepisce ogni evento che avviena nella directory osservata
+    # recepisce ogni evento che avviene nella directory osservata
     def on_any_event(self, event):
+        """Override method used as entrypoint to track filesystem events"""
         # variabile locale per controllare se un evento riguarda
         # una directory
-        isADir = event.is_directory
+        is_a_dir = event.is_directory
         # variabile locale per controllare se l'evento in questione
         # è una modifica
-        isModified = (event.event_type == 'modified')
-        # in caso si tratti di un vetento modifica di una directory
+        is_modified = (event.event_type == 'modified')
+        # in caso si tratti di un evento modifica di una directory
         # lo ignoriamo, non è interessante per quello che vogliamo
         # fare in questo programma
-        if not (isADir and isModified):
+        if not (is_a_dir and is_modified):
             # chiamata alla funzione cuore della classe
             self.process(event)
