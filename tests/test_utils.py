d9dbc362368ae19ba97e8b6cd09c96894c34c053
Davide Olgiati <davide.olgiati@hotmail.it>
2022-02-16 21:23:01 +0100

    Test fix test macOS

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
-import sys
+import platform
 import tempfile
-
 
 def temp_dirs(func):
     """Decorator to generate temporary directories."""
 
     def wrap(*args, **kwargs):
-        with tempfile.TemporaryDirectory() as source:
-            with tempfile.TemporaryDirectory() as destination:
+        tempdir = "/tmp" if platform.system() == "Darwin" else None
+        with tempfile.TemporaryDirectory(dir=tempdir) as source:
+            with tempfile.TemporaryDirectory(dir=tempdir) as destination:
                 func(*args, **kwargs, source=str(source), destination=str(destination))
 
     return wrap
